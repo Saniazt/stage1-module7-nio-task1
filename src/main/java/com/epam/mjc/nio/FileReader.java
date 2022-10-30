aeb30dd6a6065b9ddff8c38836ae177cdfb3d121
saniazt <saniazt@gmail.com>
2022-10-30 15:42:38 +0200

    1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,63 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.InputMismatchException;
 
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder name = new StringBuilder();
+        StringBuilder age = new StringBuilder();
+        StringBuilder email = new StringBuilder();
+        StringBuilder phone = new StringBuilder();
+        try (BufferedReader newBufferedReader = Files.newBufferedReader(file.toPath())) {
+            int c;
+            boolean keyEnd = false;
+            StringBuilder key = new StringBuilder();
+            while ((c = newBufferedReader.read()) != -1) {
+                char ch = (char) c;
+
+                switch (ch) {
+                    case ':':
+                        keyEnd = true;
+                    case ' ':
+                        continue;
+                    case '\n':
+                        keyEnd = false;
+                        key = new StringBuilder();
+                        break;
+                    default:
+                        break;
+                }
+
+                if (!keyEnd) {
+                    key.append(ch);
+                } else {
+                    switch (key.toString().strip()) {
+                        case "Name":
+                            name.append(ch);
+                            break;
+                        case "Age":
+                            age.append(ch);
+                            break;
+                        case "Email":
+                            email.append(ch);
+                            break;
+                        case "Phone":
+                            phone.append(ch);
+                            break;
+                        default:
+                            throw new InputMismatchException("Key '" + key + "' is not supported.");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name.toString(), Integer.parseInt(age.toString()), email.toString(), Long.parseLong(phone.toString()));
     }
 }
--- src/main/java/com/epam/mjc/nio/Profile.java
+++ src/main/java/com/epam/mjc/nio/Profile.java
@@ -3,7 +3,6 @@ package com.epam.mjc.nio;
 import java.util.Objects;
 
 public class Profile {
-
     private String name;
     private Integer age;
     private String email;
@@ -12,6 +11,7 @@ public class Profile {
     public Profile() {
 
     }
+
     public Profile(String name, Integer age, String email, Long phone) {
         this.name = name;
         this.age = age;
@@ -23,43 +23,28 @@ public class Profile {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public Integer getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
 
     public Long getPhone() {
         return phone;
     }
 
-    public void setPhone(Long phone) {
-        this.phone = phone;
-    }
+
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("Profile{");
-        sb.append("name='").append(name).append('\'');
-        sb.append(", age=").append(age);
-        sb.append(", email='").append(email).append('\'');
-        sb.append(", phone=").append(phone);
-        sb.append('}');
-        return sb.toString();
+        return "Profile{" + "name='" + name + '\'' + ", age=" + age + ", email='" + email + '\'' + ", phone=" + phone + '}';
     }
 
     @Override
@@ -71,10 +56,7 @@ public class Profile {
             return false;
         }
         Profile profile = (Profile) o;
-        return getName().equals(profile.getName())
-                && getAge().equals(profile.getAge())
-                && getEmail().equals(profile.getEmail())
-                && getPhone().equals(profile.getPhone());
+        return getName().equals(profile.getName()) && getAge().equals(profile.getAge()) && getEmail().equals(profile.getEmail()) && getPhone().equals(profile.getPhone());
     }
 
     @Override
